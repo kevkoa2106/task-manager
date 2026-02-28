5b910e86f568fcdcb11a19f3aabe4e0ef52330c7
khoa <khoa210611@icloud.com>
2026-02-28 22:31:50 +0700

    add iced_table as regular files

---
+++ src/iced_table/src/lib.rs
@@ -0,0 +1,483 @@
+//! A table widget for iced
+#![deny(missing_debug_implementations, missing_docs)]
+pub use style::Catalog;
+pub use table::{table, Table};
+
+mod divider;
+mod style;
+
+pub mod table {
+    //! Display rows of data into columns
+    use iced_core::widget;
+    use iced_core::{Element, Length, Padding};
+    use iced_widget::{column, container, row, scrollable, Space};
+
+    use super::divider::Divider;
+    use super::style;
+
+    /// Creates a new [`Table`] with the provided [`Column`] definitions
+    /// and [`Row`](Column::Row) data.
+    ///
+    /// `on_sync` is needed to keep the header & footer scrollables in sync with
+    /// the body scrollable. It is up to the consumer to emit a [`scroll_to`](iced_widget::scrollable::scroll_to) operation
+    /// from `update` when this message is received.
+    pub fn table<'a, Column, Row, Message, Theme>(
+        header: widget::Id,
+        body: widget::Id,
+        columns: &'a [Column],
+        rows: &'a [Row],
+        on_sync: fn(scrollable::AbsoluteOffset) -> Message,
+    ) -> Table<'a, Column, Row, Message, Theme>
+    where
+        Theme: style::Catalog + container::Catalog,
+    {
+        Table {
+            header,
+            body,
+            footer: None,
+            columns,
+            rows,
+            on_sync,
+            on_column_drag: None,
+            on_column_release: None,
+            min_width: 0.0,
+            min_column_width: 4.0,
+            divider_width: 2.0,
+            cell_padding: 4.into(),
+            style: Default::default(),
+            scrollbar: scrollable::Scrollbar::default(),
+        }
+    }
+
+    /// Defines what a column looks like for each [`Row`](Self::Row) of data.
+    pub trait Column<'a, Message, Theme, Renderer> {
+        /// A row of data.
+        type Row;
+
+        /// Define the header [`Element`] for this column.
+        fn header(&'a self, col_index: usize) -> Element<'a, Message, Theme, Renderer>;
+
+        /// Define the cell [`Element`] for this column.
+        fn cell(
+            &'a self,
+            col_index: usize,
+            row_index: usize,
+            row: &'a Self::Row,
+        ) -> Element<'a, Message, Theme, Renderer>;
+
+        /// Define the footer [`Element`] for this column.
+        fn footer(
+            &'a self,
+            _col_index: usize,
+            _rows: &'a [Self::Row],
+        ) -> Option<Element<'a, Message, Theme, Renderer>> {
+            None
+        }
+
+        /// Return the fixed width for this column.
+        fn width(&self) -> f32;
+
+        /// Return the offset of an on-going resize of this column.
+        fn resize_offset(&self) -> Option<f32>;
+    }
+
+    /// An element to display rows of data into columns.
+    #[allow(missing_debug_implementations)]
+    pub struct Table<'a, Column, Row, Message, Theme>
+    where
+        Theme: style::Catalog + container::Catalog,
+    {
+        header: widget::Id,
+        body: widget::Id,
+        footer: Option<widget::Id>,
+        columns: &'a [Column],
+        rows: &'a [Row],
+        on_sync: fn(scrollable::AbsoluteOffset) -> Message,
+        on_column_drag: Option<fn(usize, f32) -> Message>,
+        on_column_release: Option<Message>,
+        min_width: f32,
+        min_column_width: f32,
+        divider_width: f32,
+        cell_padding: Padding,
+        style: <Theme as style::Catalog>::Style,
+        scrollbar: scrollable::Scrollbar,
+    }
+
+    impl<'a, Column, Row, Message, Theme> Table<'a, Column, Row, Message, Theme>
+    where
+        Theme: style::Catalog + container::Catalog,
+    {
+        /// Sets the message that will be produced when a [`Column`] is resizing. Setting this
+        /// will enable the resizing interaction.
+        ///
+        /// `on_drag` will emit a message during an on-going resize. It is up to the consumer to return
+        /// this value for the associated column in [`Column::resize_offset`].
+        ///
+        /// `on_release` is emited when the resize is finished. It is up to the consumer to apply the last
+        /// `on_drag` offset to the column's stored width.
+        pub fn on_column_resize(
+            self,
+            on_drag: fn(usize, f32) -> Message,
+            on_release: Message,
+        ) -> Self {
+            Self {
+                on_column_drag: Some(on_drag),
+                on_column_release: Some(on_release),
+                ..self
+            }
+        }
+
+        /// Show the footer returned by [`Column::footer`].
+        pub fn footer(self, footer: widget::Id) -> Self {
+            Self {
+                footer: Some(footer),
+                ..self
+            }
+        }
+
+        /// Sets the minimum width of table.
+        ///
+        /// This is useful to use in conjuction with [`responsive`](iced_widget::responsive) to ensure
+        /// the table always fills the width of it's parent container.
+        pub fn min_width(self, min_width: f32) -> Self {
+            Self { min_width, ..self }
+        }
+
+        /// Sets the minimum width a column can be resized to.
+        pub fn min_column_width(self, min_column_width: f32) -> Self {
+            Self {
+                min_column_width,
+                ..self
+            }
+        }
+
+        /// Sets the width of the column dividers.
+        pub fn divider_width(self, divider_width: f32) -> Self {
+            Self {
+                divider_width,
+                ..self
+            }
+        }
+
+        /// Sets the [`Padding`] used inside each cell of the [`Table`].
+        pub fn cell_padding(self, cell_padding: impl Into<Padding>) -> Self {
+            Self {
+                cell_padding: cell_padding.into(),
+                ..self
+            }
+        }
+
+        /// Sets the style variant of this [`Table`].
+        pub fn style(self, style: impl Into<<Theme as style::Catalog>::Style>) -> Self {
+            Self {
+                style: style.into(),
+                ..self
+            }
+        }
+
+        ///  Sets the [`Scrollbar`](iced_widget::scrollable::Scrollbar) used for the table's body scrollable.
+        pub fn scrollbar(self, scrollbar: scrollable::Scrollbar) -> Self {
+            Self { scrollbar, ..self }
+        }
+    }
+
+    impl<'a, Column, Row, Message, Theme, Renderer> From<Table<'a, Column, Row, Message, Theme>>
+        for Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + iced_core::text::Renderer + 'a,
+        Theme: style::Catalog + container::Catalog + scrollable::Catalog + 'a,
+        Column: self::Column<'a, Message, Theme, Renderer, Row = Row>,
+        Message: 'a + Clone,
+    {
+        fn from(table: Table<'a, Column, Row, Message, Theme>) -> Self {
+            let Table {
+                header,
+                body,
+                footer,
+                columns,
+                rows,
+                on_sync,
+                on_column_drag,
+                on_column_release,
+                min_width,
+                min_column_width,
+                divider_width,
+                cell_padding,
+                style,
+                scrollbar,
+            } = table;
+
+            let header = scrollable(style::wrapper::header(
+                row(columns
+                    .iter()
+                    .enumerate()
+                    .map(|(index, column)| {
+                        header_container(
+                            index,
+                            column,
+                            on_column_drag,
+                            on_column_release.clone(),
+                            min_column_width,
+                            divider_width,
+                            cell_padding,
+                            style.clone(),
+                        )
+                    })
+                    .chain(dummy_container(columns, min_width, min_column_width))),
+                style.clone(),
+            ))
+            .id(header)
+            .direction(scrollable::Direction::Both {
+                vertical: scrollable::Scrollbar::new()
+                    .width(0)
+                    .margin(0)
+                    .scroller_width(0),
+                horizontal: scrollable::Scrollbar::new()
+                    .width(0)
+                    .margin(0)
+                    .scroller_width(0),
+            });
+
+            let body = scrollable(column(rows.iter().enumerate().map(|(row_index, _row)| {
+                style::wrapper::row(
+                    row(columns
+                        .iter()
+                        .enumerate()
+                        .map(|(col_index, column)| {
+                            body_container(
+                                col_index,
+                                row_index,
+                                column,
+                                _row,
+                                min_column_width,
+                                divider_width,
+                                cell_padding,
+                            )
+                        })
+                        .chain(dummy_container(columns, min_width, min_column_width))),
+                    style.clone(),
+                    row_index,
+                )
+                .into()
+            })))
+            .id(body)
+            .on_scroll(move |viewport| {
+                let offset = viewport.absolute_offset();
+
+                (on_sync)(scrollable::AbsoluteOffset { y: 0.0, ..offset })
+            })
+            .direction(scrollable::Direction::Both {
+                horizontal: scrollbar,
+                vertical: scrollbar,
+            })
+            .height(Length::Fill);
+
+            let footer = footer.map(|footer| {
+                scrollable(style::wrapper::footer(
+                    row(columns
+                        .iter()
+                        .enumerate()
+                        .map(|(index, column)| {
+                            footer_container(
+                                index,
+                                column,
+                                rows,
+                                on_column_drag,
+                                on_column_release.clone(),
+                                min_column_width,
+                                divider_width,
+                                cell_padding,
+                                style.clone(),
+                            )
+                        })
+                        .chain(dummy_container(columns, min_width, min_column_width))),
+                    style,
+                ))
+                .id(footer)
+                .direction(scrollable::Direction::Both {
+                    vertical: scrollable::Scrollbar::new()
+                        .width(0)
+                        .margin(0)
+                        .scroller_width(0),
+                    horizontal: scrollable::Scrollbar::new()
+                        .width(0)
+                        .margin(0)
+                        .scroller_width(0),
+                })
+            });
+
+            let mut column = column![header, body];
+
+            if let Some(footer) = footer {
+                column = column.push(footer);
+            }
+
+            column.height(Length::Fill).into()
+        }
+    }
+
+    fn header_container<'a, Column, Row, Message, Theme, Renderer>(
+        index: usize,
+        column: &'a Column,
+        on_drag: Option<fn(usize, f32) -> Message>,
+        on_release: Option<Message>,
+        min_column_width: f32,
+        divider_width: f32,
+        cell_padding: Padding,
+        style: <Theme as style::Catalog>::Style,
+    ) -> Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: style::Catalog + container::Catalog + 'a,
+        Column: self::Column<'a, Message, Theme, Renderer, Row = Row>,
+        Message: 'a + Clone,
+    {
+        let content = container(column.header(index))
+            .width(Length::Fill)
+            .padding(cell_padding)
+            .into();
+
+        with_divider(
+            index,
+            column,
+            content,
+            on_drag,
+            on_release,
+            min_column_width,
+            divider_width,
+            style,
+        )
+    }
+
+    fn body_container<'a, Column, Row, Message, Theme, Renderer>(
+        col_index: usize,
+        row_index: usize,
+        column: &'a Column,
+        row: &'a Row,
+        min_column_width: f32,
+        divider_width: f32,
+        cell_padding: Padding,
+    ) -> Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: style::Catalog + container::Catalog + 'a,
+        Column: self::Column<'a, Message, Theme, Renderer, Row = Row>,
+        Message: 'a + Clone,
+    {
+        let width = column.width() + column.resize_offset().unwrap_or_default();
+
+        let content = container(column.cell(col_index, row_index, row))
+            .width(Length::Fill)
+            .padding(cell_padding);
+
+        let spacing = Space::new().width(divider_width).height(Length::Shrink);
+
+        row![content, spacing]
+            .width(width.max(min_column_width))
+            .into()
+    }
+
+    fn footer_container<'a, Column, Row, Message, Theme, Renderer>(
+        index: usize,
+        column: &'a Column,
+        rows: &'a [Row],
+        on_drag: Option<fn(usize, f32) -> Message>,
+        on_release: Option<Message>,
+        min_column_width: f32,
+        divider_width: f32,
+        cell_padding: Padding,
+        style: <Theme as style::Catalog>::Style,
+    ) -> Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: style::Catalog + container::Catalog + 'a,
+        Column: self::Column<'a, Message, Theme, Renderer, Row = Row>,
+        Message: 'a + Clone,
+    {
+        let content = if let Some(footer) = column.footer(index, rows) {
+            container(footer)
+                .width(Length::Fill)
+                .padding(cell_padding)
+                .into()
+        } else {
+            Element::from(Space::new().width(Length::Fill))
+        };
+
+        with_divider(
+            index,
+            column,
+            content,
+            on_drag,
+            on_release,
+            min_column_width,
+            divider_width,
+            style,
+        )
+    }
+
+    fn with_divider<'a, Column, Row, Message, Theme, Renderer>(
+        index: usize,
+        column: &'a Column,
+        content: Element<'a, Message, Theme, Renderer>,
+        on_drag: Option<fn(usize, f32) -> Message>,
+        on_release: Option<Message>,
+        min_column_width: f32,
+        divider_width: f32,
+        style: <Theme as style::Catalog>::Style,
+    ) -> Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: style::Catalog + container::Catalog + 'a,
+        Column: self::Column<'a, Message, Theme, Renderer, Row = Row>,
+        Message: 'a + Clone,
+    {
+        let width =
+            (column.width() + column.resize_offset().unwrap_or_default()).max(min_column_width);
+
+        if let Some((on_drag, on_release)) = on_drag.zip(on_release) {
+            let old_width = column.width();
+
+            container(Divider::new(
+                content,
+                divider_width,
+                move |offset| {
+                    let new_width = (old_width + offset).max(min_column_width);
+
+                    (on_drag)(index, new_width - old_width)
+                },
+                on_release,
+                style,
+            ))
+            .width(width)
+            .into()
+        } else {
+            row![content, Space::new().width(divider_width).height(Length::Shrink)]
+                .width(width)
+                .into()
+        }
+    }
+
+    // Used to enforce "min_width"
+    fn dummy_container<'a, Column, Row, Message, Theme, Renderer>(
+        columns: &'a [Column],
+        min_width: f32,
+        min_column_width: f32,
+    ) -> Option<Element<'a, Message, Theme, Renderer>>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: style::Catalog + container::Catalog + 'a,
+        Column: self::Column<'a, Message, Theme, Renderer, Row = Row>,
+        Message: 'a + Clone,
+    {
+        let total_width: f32 = columns
+            .iter()
+            .map(|column| {
+                (column.width() + column.resize_offset().unwrap_or_default()).max(min_column_width)
+            })
+            .sum();
+
+        let remaining = min_width - total_width;
+
+        (remaining > 0.0).then(|| container(Space::new().width(remaining)).into())
+    }
+}
---
+++ src/iced_table/src/style.rs
@@ -0,0 +1,287 @@
+use iced_widget::container;
+
+/// A set of rules that dictate the styling of a [`Table`](crate::Table).
+pub trait Catalog {
+    /// The supported style of the [`Catalog`].
+    type Style: Default + Clone;
+
+    /// The header [`Style`](iced_widget::container::Style) of the [`Catalog`].
+    fn header(&self, style: &Self::Style) -> container::Style;
+    /// The footer [`Style`](iced_widget::container::Style) of the [`Catalog`].
+    fn footer(&self, style: &Self::Style) -> container::Style;
+    /// The row [`Style`](iced_widget::container::Style) of the [`Catalog`].
+    fn row(&self, style: &Self::Style, index: usize) -> container::Style;
+    /// The divider [`Style`](iced_widget::container::Style) of the [`Catalog`].
+    fn divider(&self, style: &Self::Style, hovered: bool) -> container::Style;
+}
+
+impl Catalog for iced_core::Theme {
+    type Style = ();
+
+    fn header(&self, _style: &Self::Style) -> container::Style {
+        container::Style {
+            text_color: Some(self.extended_palette().background.strong.text),
+            background: Some(self.extended_palette().background.strong.color.into()),
+            ..Default::default()
+        }
+    }
+
+    fn footer(&self, style: &Self::Style) -> container::Style {
+        self.header(style)
+    }
+
+    fn row(&self, _style: &Self::Style, index: usize) -> container::Style {
+        let pair = if index % 2 == 0 {
+            self.extended_palette().background.base
+        } else {
+            self.extended_palette().background.weak
+        };
+
+        container::Style {
+            text_color: Some(pair.text),
+            background: Some(pair.color.into()),
+            ..Default::default()
+        }
+    }
+
+    fn divider(&self, _style: &Self::Style, hovered: bool) -> container::Style {
+        let pair = if hovered {
+            self.extended_palette().primary.base
+        } else {
+            self.extended_palette().background.weak
+        };
+
+        container::Style {
+            background: Some(pair.color.into()),
+            ..Default::default()
+        }
+    }
+}
+
+pub(crate) mod wrapper {
+    use iced_core::{mouse::Cursor, Color, Element, Length, Size, Vector, Widget};
+    use iced_widget::container;
+
+    pub fn header<'a, Message, Theme, Renderer>(
+        content: impl Into<Element<'a, Message, Theme, Renderer>>,
+        style: <Theme as super::Catalog>::Style,
+    ) -> Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: super::Catalog + 'a,
+        Message: 'a,
+    {
+        Wrapper {
+            content: content.into(),
+            target: Target::Header,
+            style,
+        }
+        .into()
+    }
+
+    pub fn footer<'a, Message, Theme, Renderer>(
+        content: impl Into<Element<'a, Message, Theme, Renderer>>,
+        style: <Theme as super::Catalog>::Style,
+    ) -> Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: super::Catalog + 'a,
+        Message: 'a,
+    {
+        Wrapper {
+            content: content.into(),
+            target: Target::Footer,
+            style,
+        }
+        .into()
+    }
+
+    pub fn row<'a, Message, Theme, Renderer>(
+        content: impl Into<Element<'a, Message, Theme, Renderer>>,
+        style: <Theme as super::Catalog>::Style,
+        index: usize,
+    ) -> Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: super::Catalog + 'a,
+        Message: 'a,
+    {
+        Wrapper {
+            content: content.into(),
+            target: Target::Row { index },
+            style,
+        }
+        .into()
+    }
+
+    enum Target {
+        Header,
+        Footer,
+        Row { index: usize },
+    }
+
+    impl Target {
+        fn appearance<Theme>(
+            &self,
+            theme: &Theme,
+            style: &<Theme as super::Catalog>::Style,
+        ) -> container::Style
+        where
+            Theme: super::Catalog,
+        {
+            match self {
+                Target::Header => theme.header(style),
+                Target::Footer => theme.footer(style),
+                Target::Row { index } => theme.row(style, *index),
+            }
+        }
+    }
+
+    struct Wrapper<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer,
+        Theme: super::Catalog,
+    {
+        content: Element<'a, Message, Theme, Renderer>,
+        target: Target,
+        style: <Theme as super::Catalog>::Style,
+    }
+
+    impl<'a, Message, Theme, Renderer> Widget<Message, Theme, Renderer>
+        for Wrapper<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer,
+        Theme: super::Catalog,
+    {
+        fn size(&self) -> Size<Length> {
+            self.content.as_widget().size()
+        }
+
+        fn layout(
+            &mut self,
+            state: &mut iced_core::widget::Tree,
+            renderer: &Renderer,
+            limits: &iced_core::layout::Limits,
+        ) -> iced_core::layout::Node {
+            self.content
+                .as_widget_mut()
+                .layout(state, renderer, limits)
+        }
+
+        fn draw(
+            &self,
+            state: &iced_core::widget::Tree,
+            renderer: &mut Renderer,
+            theme: &Theme,
+            style: &iced_core::renderer::Style,
+            layout: iced_core::Layout<'_>,
+            cursor: Cursor,
+            viewport: &iced_core::Rectangle,
+        ) {
+            let appearance = self.target.appearance::<Theme>(theme, &self.style);
+
+            renderer.fill_quad(
+                iced_core::renderer::Quad {
+                    bounds: layout.bounds(),
+                    border: appearance.border,
+                    shadow: Default::default(),
+                    snap: true,
+                },
+                appearance
+                    .background
+                    .unwrap_or_else(|| Color::TRANSPARENT.into()),
+            );
+
+            let style = appearance
+                .text_color
+                .map(|text_color| iced_core::renderer::Style { text_color })
+                .unwrap_or(*style);
+
+            self.content
+                .as_widget()
+                .draw(state, renderer, theme, &style, layout, cursor, viewport)
+        }
+
+        fn tag(&self) -> iced_core::widget::tree::Tag {
+            self.content.as_widget().tag()
+        }
+
+        fn state(&self) -> iced_core::widget::tree::State {
+            self.content.as_widget().state()
+        }
+
+        fn children(&self) -> Vec<iced_core::widget::Tree> {
+            self.content.as_widget().children()
+        }
+
+        fn diff(&self, tree: &mut iced_core::widget::Tree) {
+            self.content.as_widget().diff(tree)
+        }
+
+        fn operate(
+            &mut self,
+            state: &mut iced_core::widget::Tree,
+            layout: iced_core::Layout<'_>,
+            renderer: &Renderer,
+            operation: &mut dyn iced_core::widget::Operation,
+        ) {
+            self.content
+                .as_widget_mut()
+                .operate(state, layout, renderer, operation)
+        }
+
+        fn update(
+            &mut self,
+            state: &mut iced_core::widget::Tree,
+            event: &iced_core::Event,
+            layout: iced_core::Layout<'_>,
+            cursor: Cursor,
+            renderer: &Renderer,
+            clipboard: &mut dyn iced_core::Clipboard,
+            shell: &mut iced_core::Shell<'_, Message>,
+            viewport: &iced_core::Rectangle,
+        ) {
+            self.content.as_widget_mut().update(
+                state, event, layout, cursor, renderer, clipboard, shell, viewport,
+            );
+        }
+
+        fn mouse_interaction(
+            &self,
+            state: &iced_core::widget::Tree,
+            layout: iced_core::Layout<'_>,
+            cursor: Cursor,
+            viewport: &iced_core::Rectangle,
+            renderer: &Renderer,
+        ) -> iced_core::mouse::Interaction {
+            self.content
+                .as_widget()
+                .mouse_interaction(state, layout, cursor, viewport, renderer)
+        }
+
+        fn overlay<'b>(
+            &'b mut self,
+            state: &'b mut iced_core::widget::Tree,
+            layout: iced_core::Layout<'b>,
+            renderer: &Renderer,
+            viewport: &iced_core::Rectangle,
+            translation: Vector,
+        ) -> Option<iced_core::overlay::Element<'b, Message, Theme, Renderer>> {
+            self.content
+                .as_widget_mut()
+                .overlay(state, layout, renderer, viewport, translation)
+        }
+    }
+
+    impl<'a, Message, Theme, Renderer> From<Wrapper<'a, Message, Theme, Renderer>>
+        for Element<'a, Message, Theme, Renderer>
+    where
+        Renderer: iced_core::Renderer + 'a,
+        Theme: super::Catalog + 'a,
+        Message: 'a,
+    {
+        fn from(wrapper: Wrapper<'a, Message, Theme, Renderer>) -> Self {
+            Element::new(wrapper)
+        }
+    }
+}
